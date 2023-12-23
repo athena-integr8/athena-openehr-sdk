811ffa9a91ca21ca9a599a73ebb60207aa7c2c6e
Jacob Hoeflaken <jacob.hoeflaken@axians.com>
2023-12-23 19:43:42 +0100

    Adding REST APIs Directory.

--- athena-openehr-rest/src/main/java/io/athena/openehr/rest/api/ehr/ContributionApi.java
+++ athena-openehr-rest/src/main/java/io/athena/openehr/rest/api/ehr/ContributionApi.java
@@ -1,4 +1,18 @@
 package io.athena.openehr.rest.api.ehr;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.ws.rs.core.Response;
+
 public interface ContributionApi {
+
+    Response createContribution(
+            @NotBlank final String theEhrId,
+            @NotBlank final String thePrefer,
+            @NotNull final Contribution theContribution);
+
+    Response getContributionById(
+            @NotBlank final String theEhrId,
+            @NotBlank final String theContributionUid);
+
 }
--- athena-openehr-rest/src/main/java/io/athena/openehr/rest/api/ehr/ContributionApiAsync.java
+++ athena-openehr-rest/src/main/java/io/athena/openehr/rest/api/ehr/ContributionApiAsync.java
@@ -1,4 +1,20 @@
 package io.athena.openehr.rest.api.ehr;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.ws.rs.core.Response;
+
+import java.util.concurrent.CompletionStage;
+
 public interface ContributionApiAsync {
+
+    CompletionStage<Response> createContribution(
+            @NotBlank final String theEhrId,
+            @NotBlank final String thePrefer,
+            @NotNull final Contribution theContribution);
+
+    CompletionStage<Response> getContributionById(
+            @NotBlank final String theEhrId,
+            @NotBlank final String theContributionUid);
+
 }
