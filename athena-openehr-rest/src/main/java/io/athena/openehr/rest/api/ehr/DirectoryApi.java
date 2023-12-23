b3fc1aa08f90410885ce1c75ea1ae4db2ce43cde
Jacob Hoeflaken <jacob.hoeflaken@axians.com>
2023-12-23 19:40:22 +0100

    Adding REST APIs Directory.

--- athena-openehr-rest/src/main/java/io/athena/openehr/rest/api/ehr/DirectoryApi.java
+++ athena-openehr-rest/src/main/java/io/athena/openehr/rest/api/ehr/DirectoryApi.java
@@ -1,4 +1,35 @@
 package io.athena.openehr.rest.api.ehr;
 
+import io.openehr.rm.Folder;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.ws.rs.core.Response;
+
 public interface DirectoryApi {
+
+    Response createDirectory(
+            @NotBlank final String theEhrId,
+            @NotBlank final String thePrefer,
+            @NotNull final Folder theDirectory);
+
+    Response updateDirectory(
+            @NotBlank final String theEhrId,
+            @NotBlank final String theIfMatch,
+            @NotBlank final String thePrefer,
+            @NotNull final Folder theDirectory);
+
+    Response deleteDirectory(
+            @NotBlank final String theEhrId,
+            @NotBlank final String theIfMatch);
+
+    Response getFolderInDirectoryVersionAtTime(
+            @NotBlank final String theEhrId,
+            @NotBlank final String theVersionAtTime,
+            @NotBlank final String thePath);
+
+    Response getFolderInDirectoryVersion(
+            @NotBlank final String theEhrId,
+            @NotBlank final String theVersionUid,
+            @NotBlank final String thePath);
+
 }
--- athena-openehr-rest/src/main/java/io/athena/openehr/rest/api/ehr/DirectoryApiAsync.java
+++ athena-openehr-rest/src/main/java/io/athena/openehr/rest/api/ehr/DirectoryApiAsync.java
@@ -1,4 +1,37 @@
 package io.athena.openehr.rest.api.ehr;
 
+import io.openehr.rm.Folder;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.ws.rs.core.Response;
+
+import java.util.concurrent.CompletionStage;
+
 public interface DirectoryApiAsync {
+
+    CompletionStage<Response> createDirectory(
+            @NotBlank final String theEhrId,
+            @NotBlank final String thePrefer,
+            @NotNull final Folder theDirectory);
+
+    CompletionStage<Response> updateDirectory(
+            @NotBlank final String theEhrId,
+            @NotBlank final String theIfMatch,
+            @NotBlank final String thePrefer,
+            @NotNull final Folder theDirectory);
+
+    CompletionStage<Response> deleteDirectory(
+            @NotBlank final String theEhrId,
+            @NotBlank final String theIfMatch);
+
+    CompletionStage<Response> getFolderInDirectoryVersionAtTime(
+            @NotBlank final String theEhrId,
+            @NotBlank final String theVersionAtTime,
+            @NotBlank final String thePath);
+
+    CompletionStage<Response> getFolderInDirectoryVersion(
+            @NotBlank final String theEhrId,
+            @NotBlank final String theVersionUid,
+            @NotBlank final String thePath);
+
 }
